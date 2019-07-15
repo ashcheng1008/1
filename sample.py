e67aa760c4ed8781cad8fcc3a8424eef6a870524
ashcheng1008 <52899007+ashcheng1008@users.noreply.github.com>
2019-07-15 11:32:52 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -44,8 +44,8 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))'''
     message=StickerSendMessage(
-    Package_id='1',
-    Sticker_id='1'
+        package_id='1',
+        sticker_id='1'
     )
     line_bot_api.reply_message(event.reply_token,message)
 
