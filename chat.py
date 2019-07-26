aa8613c8db525d44cbfca36eb1b487b56037e807
ashcheng1008 <52899007+ashcheng1008@users.noreply.github.com>
2019-07-26 11:25:47 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,17 +1,17 @@
 if lineMessage[0:3]==“看醫生”:
   address=“”
   lineMes=lineMessage
-  if  lineMes[4:-1]==“”:
-      address="桃園市"
+  if lineMes[4:-1]==“”:
+    address="桃園市"
   else:
-      address=lineMes[4:-1]
-  addurl=‘https://maps.googleapls.com/maps/api/getcode/json?key={}&address={}&sensor=false’.format(GOOGLE_API_KEY,address)
-  addressReq=requests.get(addurl)
-  addressDoc=addressReq.json()
-  lat=addreeDoc[‘results’][0][‘geometry’][‘location’][‘lat’]
-  lng=addreeDoc[‘results’][0][‘geometry’][‘location’][‘lng’]
+    address=lineMes[4:-1]
+    addurl=‘https://maps.googleapis.com/maps/api/getcode/json?key={}&address={}&sensor=false’.format(AIzaSyBKhoq1t6NEHtpbq2ANkrqa8ClnBCzbiWs,address)
+    addressReq=requests.get(addurl)
+    addressDoc=addressReq.json()
+    lat=addreeDoc[‘results’][0][‘geometry’][‘location’][‘lat’]
+    lng=addreeDoc[‘results’][0][‘geometry’][‘location’][‘lng’]
 
-medsearch=‘https://maps.googleapls.com/maps/api/getcode/json?key={}&address={}&sensor=false’.format(GOOGLE_API_KEY,lat,lng)
+medsearch=‘https://maps.googleapis.com/maps/api/getcode/json?key={}&address={}&sensor=false’.format(AIzaSyBKhoq1t6NEHtpbq2ANkrqa8ClnBCzbiWs,lat,lng)
 medReq=requests.get(medsearch)
 nearby_clinic_dict=medReq.json()
 top20_restaurants=nearby_clinic_dict[“results”]
@@ -23,7 +23,7 @@ if clinic.get(“photos”) is None:
 else:
   photo_reference=clinic[“photos”][0][“photo_reference”]
   Photo_width=clinic[“photos”][0][“width”]
-  thumbnail_image_url=“https://maps.googleapls.com/maps/api/place/photo?key={}&photoreference={}&maxwidth={}”.format(GOOGLE_API_KEY,photo_reference,photo_width)
+  thumbnail_image_url=“https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth={}”.format(AIzaSyBKhoq1t6NEHtpbq2ANkrqa8ClnBCzbiWs,photo_reference,photo_width)
 
 address=“沒有資料” if clinic.get(“vicinity”) is None else clinic[“vicinity”]
 map_url=“https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}={place_id}”.format(lat=clinic[“geometry”][“location”][“lat”],long=clinic[“geometry”][“location”][“lng”],place_id=clinic[“place_id”])
@@ -31,16 +31,16 @@ map_url=“https://www.google.com/maps/search/?api=1&query={lat},{long}&query_pl
 buttons_template=TemplateSendMessage(
 alt_text=clinic[“name”],
 template=ButtonsTemplate(
-    thumbnail_image_url=thumbnail_image_url,
-    title=restaurant[“name”],
-    text=details,
-    actions=[
-      URITemplateAction(
-        label=‘查看地圖’,
-        uri=map_url
-      ),
-    ]
-  )
+  thumbnail_image_url=thumbnail_image_url,
+  title=restaurant[“name”],
+  text=details,
+  actions=[
+    URITemplateAction(
+      label=‘查看地圖’,
+      uri=map_url
+    ),
+  ]
+)
 )
 line_bot_api.reply_message(event.reply_token,buttons_template)
 Return 0
