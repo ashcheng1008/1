46c56bdc83ba7ee5ec9d425727accd185ca13dab
ashcheng1008 <52899007+ashcheng1008@users.noreply.github.com>
2019-07-26 11:59:46 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,46 +1,63 @@
-if lineMessage[0:3]==“看醫生”:
-  address=“”
-  lineMes=lineMessage
-  if lineMes[4:-1]==“”:
-    address="桃園市"
-  else:
-    address=lineMes[4:-1]
-    addurl=‘https://maps.googleapis.com/maps/api/getcode/json?key={}&address={}&sensor=false’.format(AIzaSyBKhoq1t6NEHtpbq2ANkrqa8ClnBCzbiWs,address)
-    addressReq=requests.get(addurl)
-    addressDoc=addressReq.json()
-    lat=addreeDoc[‘results’][0][‘geometry’][‘location’][‘lat’]
-    lng=addreeDoc[‘results’][0][‘geometry’][‘location’][‘lng’]
+import requests
+import re
+import configparser
+import json
+from bs4 import BeautifulSoup
+import random
 
-medsearch=‘https://maps.googleapis.com/maps/api/getcode/json?key={}&address={}&sensor=false’.format(AIzaSyBKhoq1t6NEHtpbq2ANkrqa8ClnBCzbiWs,lat,lng)
-medReq=requests.get(medsearch)
-nearby_clinic_dict=medReq.json()
-top20_restaurants=nearby_clinic_dict[“results”]
-cli_num=(len(top_clinic))
+# 預設地址
+address = ''
+# 你的API_KEY
+GOOGLE_API_KEY = 'AIzaSyBKhoq1t6NEHtpbq2ANkrqa8ClnBCzbiWs'
 
-clinic=random.choice(top_client)
-if clinic.get(“photos”) is None:
-  thumbnail_image)url=None
+addurl = 'https://maps.googleapis.com/maps/api/geocode/json?key={}&address={}&sensor=false'.format(GOOGLE_API_KEY,address)
+
+# 經緯度轉換
+addressReq = requests.get(addurl)
+addressDoc = addressReq.json()
+lat = addressDoc['results'][0]['geometry']['location']['lat']
+lng = addressDoc['results'][0]['geometry']['location']['lng']
+
+# 取得店家資訊
+MedSearch = "https://www.google.com/maps/search/醫院/@24.8524406,121.2282846,14z/data=!3m1!4b1.format(GOOGLE_API_KEY, lat, lng)
+
+clinicReq = requests.get(MedSearch)
+nearbyclinic_dict = foodReq.json()
+
+top20Restaurants = nearbyRestaurants_dict["results"]
+res_num = (len(top20Restaurants)) 
+# 取評分高於3.9的店家
+bravo=[]
+for i in range(res_num):
+  try:
+    if top20Restaurants[i]['rating'] > 3.9:
+      # print('rate: ', top20Restaurants[i]['rating'])
+      bravo.append(i)
+  except:
+    KeyError
+if len(bravo) < 0:
+  print("沒東西可以吃")
+  # restaurant = random.choice(top20Restaurants) 沒有的話隨便選一間
+
+# 從高於3.9的店家隨機選一間
+restaurant = top20Restaurants[random.choice(bravo)]
+
+# 檢查餐廳有沒有照片
+if restaurant.get("photos") is None:
+  thumbnailImageUrl = None
 else:
-  photo_reference=clinic[“photos”][0][“photo_reference”]
-  Photo_width=clinic[“photos”][0][“width”]
-  thumbnail_image_url=“https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth={}”.format(AIzaSyBKhoq1t6NEHtpbq2ANkrqa8ClnBCzbiWs,photo_reference,photo_width)
+  # 取得照片
+  photoReference = restaurant["photos"][0]["photo_reference"]
+  photoWidth = restaurant["photos"][0]["width"]
+  thumbnailImageUrl = "https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth={}".format(GOOGLE_API_KEY, photoReference,photoWidth)
+  
+# 餐廳詳細資訊
+rating = "無" if restaurant.get("rating") is None else restaurant["rating"]
+address = "沒有資料" if restaurant.get("vicinity") is None else restaurant["vicinity"]
+details = "Google Map評分：{}\n地址：{}".format(rating, address)
+print(restaurant.get("name"))
+print(details)
 
-address=“沒有資料” if clinic.get(“vicinity”) is None else clinic[“vicinity”]
-map_url=“https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}={place_id}”.format(lat=clinic[“geometry”][“location”][“lat”],long=clinic[“geometry”][“location”][“lng”],place_id=clinic[“place_id”])
-
-buttons_template=TemplateSendMessage(
-alt_text=clinic[“name”],
-template=ButtonsTemplate(
-  thumbnail_image_url=thumbnail_image_url,
-  title=restaurant[“name”],
-  text=details,
-  actions=[
-    URITemplateAction(
-      label=‘查看地圖’,
-      uri=map_url
-    ),
-  ]
-)
-)
-line_bot_api.reply_message(event.reply_token,buttons_template)
-return 0
+ # 取得餐廳的 Google map 網址
+mapUrl = "https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(lat=restaurant["geometry"]["location"]["lat"],long=restaurant["geometry"]["location"]["lng"],place_id=restaurant["place_id"])
+print(mapUrl)
