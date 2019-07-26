4c5794d73516247c265f0c196dc8fed965a403a7
ashcheng1008 <52899007+ashcheng1008@users.noreply.github.com>
2019-07-26 16:22:51 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -38,12 +38,47 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # 回應使用者輸入的話
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
+# 傳送訊息到 line
+  def reply_to_line(reply_text)
+    # 取得 reply token
+    reply_token = params['events'][0]['replyToken']
+    
+    # 設定回覆訊息
+    message = {
+        "type": "imagemap",
+        "baseUrl": "https://www.google.com/maps/uv?hl=zh-TW&pb=!1s0x34682231ba47afbd%3A0xdea0c1705504bd52!2m22!2m2!1i80!2i80!3m1!2i20!16m16!1b1!2m2!1m1!1e1!2m2!1m1!1e3!2m2!1m1!1e5!2m2!1m1!1e4!2m2!1m1!1e6!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOGS7yb2HwrJ1d0GFmpe8ReWONc5xP4ORfiXfqP%3Dw319-h320-k-no!5z5YSE6LuS5Lit6Yar6Ki65omAIC0gR29vZ2xlIOaQnOWwiw!15sCAQ&imagekey=!1e10!2sAF1QipOGS7yb2HwrJ1d0GFmpe8ReWONc5xP4ORfiXfqP&sa=X&ved=2ahUKEwiCleeS3M_jAhXJxosBHRt2CosQoiowDHoECAsQBg#",
+        "altText": "億軒中醫診所1",
+        "baseSize": {
+            "height": 1040,
+            "width": 1040
+        },
+        "actions": [
+            {
+                "type": "uri",
+                "linkUri": "http://yi-hsuan.tcm.tw",
+                "label": "億軒中醫診所2",
+                "area": {
+                    "x": 0,
+                    "y": 0,
+                    "width": 520,
+                    "height": 1040
+                }
+            },
+            {
+                "type": "message",
+                "text": "億軒中醫診所3",
+                "area": {
+                    "x": 520,
+                    "y": 0,
+                    "width": 520,
+                    "height": 1040
+                }
+            }
+        ]
+    }
+    # 傳送訊息
+    line.reply_message(reply_token, message)
+  end
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
